70329deb94ce80a4ed004412b88637b96b939f69
GFM97 <33311109+GFM97@users.noreply.github.com>
2018-03-20 15:30:51 +0100

    Gameplay Basics
    - Player Movement
    - Camera follow

---
+++ Video Game/Assets/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	// This is a reference to the Rigidbody component called "rb"
+	public Rigidbody rb;
+
+	public float forwardForce = 2000f;
+	public float sidewaysForce = 500f;
+
+	// We marked this as "Fixed"Update because we 
+	// are using it to mess with physics.
+	void FixedUpdate ()
+	{
+		// Add a forward force
+		rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+		if(Input.GetKey(KeyCode.D))
+		{
+			rb.AddForce (sidewaysForce * Time.deltaTime, 0, 0);
+		}
+
+		if(Input.GetKey(KeyCode.A))
+		{
+			rb.AddForce (-sidewaysForce * Time.deltaTime, 0, 0);
+		}
+	}
+}
