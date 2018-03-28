8d3aff190f2a42bc6855c8497e7840349c9bcca1
GFM97 <33311109+GFM97@users.noreply.github.com>
2018-03-28 19:03:00 +0200

    Menus, Buttons, Sound, Finsih Line
    - Clear Player Prefs.
    - Pause menu including Resume, Mute and Menu buttons.
    - Start menu including Start, Level, Mute and Quit buttons.
    - Level menu to select levels from 1 to 6.
    - Sound including introduction music, background music and hitting an object sound.
    - Finish line at the end of each level.

---
+++ CUBECRASH/Assets/Scripts/LevelAccess.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelAccess : MonoBehaviour {
+
+	public int level;
+	public GameObject image;
+
+	void Awake()
+	{
+		bool active = PlayerPrefs.GetInt ("level") <= level;
+		image.SetActive (active);
+
+		GetComponent<Button> ().interactable = active;
+	}
+
+}
